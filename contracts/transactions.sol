b296a243aaa213ee13d9db268bbe2157e8b1e8b4
Talal <alamtalal004@gmail.com>
2023-06-28 09:17:32 -0400

    fixed a bug in the sufficientBalance mod, added al the blockedMoney var, made some var as public

--- contracts/transactions.sol
+++ contracts/transactions.sol
@@ -1,7 +1,7 @@
 pragma solidity >=0.4.22 <=0.8.19;
 
 /* 
-We should try to change all the string[] of motorIds and the strings of motorId 
+We should try to change all the string[] of motorIDS and the strings of motorId 
 to a uint type array or var. Maybe with a function that converts string to unique 
 number with a key like hashing it would be possible. The thing is that i don't know if 
 we should do it at the contract level or more like at consuming the contract and sending the 
@@ -66,20 +66,22 @@ contract CarTransactions {
     }
 
     //the motor id of all the cars that are in the contract (either sold or not)
-    string[] motorIds;
+    string[] public  motorIDS;
     //All the cars in the contract, the string will be the motorId
-    mapping(string => Car) cars;
+    mapping(string => Car) public cars;
     //All the buyers in the contract
-    mapping(address => Buyer) buyers;
+    mapping(address => Buyer) private  buyers;
     //All the sellers in the contract
-    mapping(address => Seller) sellers;
+    mapping(address => Seller) private sellers;
     //All the money that is waiting for the buyer confirming 
     //(address: buyer, string : motorId, uint: amount of money)
-    mapping(address => mapping(string => uint256)) blockedMoney;
+    mapping(address => mapping(string => uint256)) public blockedMoney;
+    //All teh blocked money that the contract has
+    uint256 public allBlockedMoney;
 
     //makes sure that the amount sent is correct
-    modifier suficcientBalance(uint256 price){
-        require(price == msg.value,"You did not passed the right amount of money");
+    modifier suficcientBalance(string memory motorId){
+        require(cars[motorId].price == msg.value,"You did not passed the right amount of money");
         _;
     }
 
@@ -89,10 +91,11 @@ contract CarTransactions {
     }
 
     function sellCar(string memory motorId, uint256 price) public{
+        require(price > 0 ,"Please provide a price");
         //Adding to seller's sold cars
         sellers[msg.sender].soldCars.push(motorId);
         //Adding to the contract total cars 
-        motorIds.push(motorId);
+        motorIDS.push(motorId);
         //Creating the cars struct 
         cars[motorId].motorId = motorId;
         cars[motorId].price = price;
@@ -100,7 +103,7 @@ contract CarTransactions {
         emit CarAddedToSale(msg.sender, price, motorId, block.timestamp);
     }
 
-    function buyCar(string memory motorId) external payable suficcientBalance(msg.value) {
+    function buyCar(string memory motorId) external payable suficcientBalance(motorId) {
         require(!cars[motorId].sold, "The car is sold");
 
         //modifying the car struct 
@@ -108,8 +111,11 @@ contract CarTransactions {
         cars[motorId].buyer = msg.sender;
         //Adding blockedMoney to the seller
         sellers[cars[motorId].seller].blockedMoney += msg.value;
+        //Adding blockedMoney to the buyer 
+        buyers[msg.sender].blockedMoney += msg.value;
         //Adding money to the contract total amount of blocked money
         blockedMoney[msg.sender][motorId] += msg.value;
+        allBlockedMoney += msg.value;
 
         emit CarBought(msg.sender, cars[motorId].seller, msg.value, motorId);
     }
@@ -127,6 +133,7 @@ contract CarTransactions {
         carBuyer.spentMoney += money;
         carBuyer.boughtCars.push(motorId);
         //decreasing the amount of money that blocked money has 
+        allBlockedMoney -= money;
         delete blockedMoney[msg.sender][motorId];
         emit CarDelivered(block.timestamp);
     }
@@ -140,12 +147,7 @@ contract CarTransactions {
         emit Withdraw(amount, block.timestamp);
     }
 
-    function getAllCars() public view returns(string[] memory){
-        return motorIds;
-    }
-
     function getUserSoldCars() public view returns(string[] memory){
         return sellers[msg.sender].soldCars;
     }  
-
 }
